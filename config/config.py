b80ddabbc9a8e56dd103181f1c15ecb2901c2abc
masiur2 <57829382+masiur2@users.noreply.github.com>
2022-07-17 11:55:59 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -57,10 +57,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "DRAGO_CHEATS"
+    "SUPPORT_CHANNEL", "https://t.me/DRAGO_CHEATS"
 )
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "DRAGO_MOD_CHAT"
+    "SUPPORT_GROUP", "https://t.me/DRAGO_MOD_CHAT"
 )  
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
